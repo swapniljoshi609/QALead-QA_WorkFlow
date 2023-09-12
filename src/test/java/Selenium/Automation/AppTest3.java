93f85079eb6c848dc825ade6031579f93a508525
swapnil <joshiswapnil609@gmail.com>
2023-09-12 17:58:33 +0530

    More TC added

---
+++ src/test/java/Selenium/Automation/AppTest3.java
@@ -0,0 +1,10 @@
+package Selenium.Automation;
+
+
+public class AppTest3 
+{
+   public static void main(String[] args) {
+	
+	   System.out.println("TC1");
+}
+}
---
+++ src/test/java/Selenium/Automation/AppTest4.java
@@ -0,0 +1,10 @@
+package Selenium.Automation;
+
+
+public class AppTest4 
+{
+   public static void main(String[] args) {
+	
+	   System.out.println("TC1");
+}
+}
